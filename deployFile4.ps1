c16bb3978b4031c4d57ef6af64fbb63ca62d5055
prak96 <p1@gmail.com>
2024-07-09 02:16:04 +0530

    configuring bastion..

--- deployFile4.ps1
+++ deployFile4.ps1
@@ -1,11 +1,11 @@
-$template = "D:\VS CoDe\Project\Insightscape\InsightScape\insightmain.json"
-$templatepara = "D:\VS CoDe\Project\Insightscape\InsightScape\insightmain.parameter.json"
-$rg = 'netbridge-rg'
+$template = "D:\VS CoDe\Project\netBridge\netBridge\netbridge.json"
+$templatepara = "D:\VS CoDe\Project\netBridge\netBridge\netbridge.parameter.json"
+$rg = 'automate-rg'
 $loc = 'eastasia'
 
 New-AzResourceGroup -Name $rg -Location $loc
 New-AzResourceGroupDeployment `
--Name netdeploy `
+-Name netdeploy6 `
 -ResourceGroupName $rg `
 -TemplateFile $template `
 -TemplateParameterFile $templatepara -verbose
