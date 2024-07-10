144bb808c1c8e76807b5c410e511e7992014bd1e
prak96 <p1@gmail.com>
2024-07-10 14:29:40 +0530

    changes-v10.7.24

--- deployFile4.ps1
+++ deployFile4.ps1
@@ -1,11 +1,13 @@
 $template = "D:\VS CoDe\Project\netBridge\netBridge\netbridge.json"
 $templatepara = "D:\VS CoDe\Project\netBridge\netBridge\netbridge.parameter.json"
-$rg = 'automate-rg'
-$loc = 'eastasia'
+$rg = 'pkp-rg'
 
-New-AzResourceGroup -Name $rg -Location $loc
 New-AzResourceGroupDeployment `
--Name netdeploy6 `
+-Name netbridgeDeploy `
 -ResourceGroupName $rg `
 -TemplateFile $template `
 -TemplateParameterFile $templatepara -verbose
+
+
+Remove-AzResourceGroup pkp-rg -Force
+
