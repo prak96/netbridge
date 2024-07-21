af8be2d37fcf5890f12df3061794758d5ddeb6ad
prak96 <p1@gmail.com>
2024-07-21 11:17:28 +0530

    Complete Code main

--- deployFile4.ps1
+++ deployFile4.ps1
@@ -1,12 +1,13 @@
 $template = "D:\VS CoDe\Project\netBridge\netBridge\netbridge.json"
 $templatepara = "D:\VS CoDe\Project\netBridge\netBridge\netbridge.parameter.json"
-$rg = 'pkp-rg'
+$rg = 'netbridge-rg'
+$loc= 'eastasia'
+
+New-AzResourceGroup -Name $rg -Location $loc
 
 New-AzResourceGroupDeployment `
 -Name netbridgeDeploy `
 -ResourceGroupName $rg `
 -TemplateFile $template `
 -TemplateParameterFile $templatepara -verbose
-
-
 
